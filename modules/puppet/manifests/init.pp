6c790daf87d6eabfb3c79b26271130577d1c8530
micke <mikael.lundmark@gmail.com>
2014-03-02 23:26:32 +0100

    puppet init.pp

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,6 +1,25 @@
 class puppet {
   file { '/usr/local/bin/papply':
-   source => 'puppet:///modules/puppet/papply.sh',
-   mode   => '0755',
+  source => 'puppet:///modules/puppet/papply.sh',
+  mode   => '0755',
+  }
+
+  file { '/usr/local/bin/pull-updates':
+  source => 'puppet:///modules/puppet/pull-updates.sh',
+  mode   => '0755',
+  }
+
+  file { '/home/lunken/.ssh/id_rsa':
+  source => 'puppet:///modules/puppet/lunken.priv',
+  owner  => 'lunken',
+  mode   => '0600',
+  }
+
+  cron { 'run-puppet':
+  ensure  => 'present',
+  user    => 'lunken',
+  command => '/usr/local/bin/pull-updates',
+  minute  => '*/10',
+  hour    => '*',
   }
 }
